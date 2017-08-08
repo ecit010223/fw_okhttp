b6d7da33f508e671b00af98e1302739564c9a089
zyh2017 <121583054@qq.com>
2017-08-08 18:04:41 +0800

    GET请求

---
+++ app/src/main/java/com/year17/fw_okhttp/Constants.java
@@ -0,0 +1,12 @@
+package com.year17.fw_okhttp;
+
+/**
+ * 作者：张玉辉
+ * 时间：2017/8/8.
+ */
+
+public class Constants {
+    public static final String URL = "http://192.168.1.211:9000/rxjava";
+    public static final int INTERNET_REQUEST_CODE = 1;
+    public static final String TAG = "tag";
+}
